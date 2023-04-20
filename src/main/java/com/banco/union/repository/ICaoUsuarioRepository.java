8aa91c7dec3f17b9e16c372bf5912b2b90a92a8a
Alex Herrera <estudiantepb08@gmail.com>
2023-04-19 22:43:19 -0500

    Confi new database

--- src/main/java/com/banco/union/repository/ICaoUsuarioRepository.java
+++ src/main/java/com/banco/union/repository/ICaoUsuarioRepository.java
@@ -15,8 +15,8 @@ import com.banco.union.models.entity.CaoUsuarioPk;
 @Repository
 public interface ICaoUsuarioRepository extends CrudRepository<CaoUsuario, CaoUsuarioPk>{
 	
-	@Query(value = " SELECT cau.no_usuario AS noUsuario FROM caol.permissao_sistema ps " +
-			" CROSS JOIN caol.cao_usuario cau ON cau.co_usuario = ps.co_usuario " +
+	@Query(value = " SELECT cau.no_usuario AS noUsuario FROM permissao_sistema ps " +
+			" CROSS JOIN cao_usuario cau ON cau.co_usuario = ps.co_usuario " +
 			" WHERE ps.co_sistema = :coSistema AND ps.in_ativo = :inActivo AND ps.co_tipo_usuario IN (:coTipoUsuarios) ", nativeQuery = true)	
 	List<ICaoUsuarioDto> findByNoUsuario(@Param("coSistema") Integer coSistema, @Param("inActivo") char inActivo, @Param("coTipoUsuarios") List<Integer> coTipoUsuarios);
 
